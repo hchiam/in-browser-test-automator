0d0aec9be649487809ce394c73c83a5ecefa76b2
hchiam <hchiam@users.noreply.github.com>
2019-04-19 22:56:46 -0400

    add some jquery

--- popup.js
+++ popup.js
@@ -1,8 +1,43 @@
-'use strict';
+(function() {
 
-let runButton = document.getElementById('start-button');
+	var numberOfStepsCreated = 0;
+	addStep();
+	function addStep() {
+		numberOfStepsCreated++;
+		$('#steps').add(`
+		<div id="step-${numberOfStepsCreated}">
+			<select class="action">
+				<option value="click">Click on</option>
+				<option value="select">Select</option>
+				<option value="enter">Enter</option>
+				<option value="check">Should show</option>
+			</select> : 
+			<input placeholder="">
+			<button id="remove-step-${numberOfStepsCreated}">-</button>
+		</div>
+		`).appendTo('#steps');
 
-runButton.addEventListener("click", function useSettings() {
-  chrome.tabs.executeScript(null, {file: 'test.js'});
-  window.close();
-});
+		$('#remove-step-' + numberOfStepsCreated).click(function useSettings(e) {
+			var step = e.target.id.replace(/^remove-step-/,'');
+			$("#step-" + step).remove();
+		})
+	}
+
+	$('#start-button').click(function useSettings() {
+		chrome.tabs.executeScript(null, {file: 'test.js'});
+	})
+
+	$('#add-step').click(function useSettings() {
+		addStep();
+	})
+
+})();
+
+
+document.addEventListener('contextmenu', function autoFillClickIdentifier(event) {
+	var e = event.target;
+	var tag = (e.tagName) ? e.tagName.toLowerCase() : '';
+	var id = (e.id) ? '#' + e.id : '';
+	var classes = (e.className) ? '.' + e.className.replace(' ','.') : '';
+	alert(tag + id + classes);
+}, false);

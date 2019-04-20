a994220e36b5f68ec3a9ddb364001a8816972e55
hchiam <hchiam@users.noreply.github.com>
2019-04-19 23:42:08 -0400

    cleanup - no need for jquery

--- popup.js
+++ popup.js
@@ -1,34 +1,6 @@
-(function() {
+var runButton = document.getElementById('start-button');
 
-	var numberOfStepsCreated = 0;
-	addStep();
-	function addStep() {
-		numberOfStepsCreated++;
-		$('#steps').add(`
-		<div id="step-${numberOfStepsCreated}">
-			<select class="action">
-				<option value="click">Click on</option>
-				<option value="select">Select</option>
-				<option value="enter">Enter</option>
-				<option value="check">Should show</option>
-			</select> : 
-			<input placeholder="">
-			<button id="remove-step-${numberOfStepsCreated}">-</button>
-		</div>
-		`).appendTo('#steps');
-
-		$('#remove-step-' + numberOfStepsCreated).click(function useSettings(e) {
-			var step = e.target.id.replace(/^remove-step-/,'');
-			$("#step-" + step).remove();
-		})
-	}
-
-	$('#start-button').click(function useSettings() {
-		chrome.tabs.executeScript(null, {file: 'test.js'});
-	})
-
-	$('#add-step').click(function useSettings() {
-		addStep();
-	})
-
-})();
+runButton.addEventListener('click', function() {
+	chrome.tabs.executeScript(null, {file: 'test.js'});
+	window.close();
+});

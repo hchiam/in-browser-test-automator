7e31d43e44275779dce821ad67021791cd00ccb8
hchiam <hchiam@users.noreply.github.com>
2019-04-26 10:46:24 -0400

    initialize numberOfStepsCreated to 1

--- popup.js
+++ popup.js
@@ -1,11 +1,4 @@
-var numberOfStepsCreated = 0;
 var savedSteps = '';
-
-chrome.storage.local.get('numberOfStepsCreated', function getSettings(data) {
-	numberOfStepsCreated = data.numberOfStepsCreated ? 'var numberOfStepsCreated = ' + numberOfStepsCreated + ';' : 
-		`var numberOfStepsCreated = 0;`;
-});
-
 chrome.storage.local.get('savedSteps', function getSettings(data) {
 	savedSteps = data.savedSteps ? 'var savedSteps = ' + JSON.stringify(data.savedSteps) + ';' : 
 		`var savedSteps = [
@@ -16,8 +9,14 @@ chrome.storage.local.get('savedSteps', function getSettings(data) {
 		];`;
 });
 
-let runButton = document.getElementById('start-button');
+var numberOfStepsCreated = 0;
+chrome.storage.local.get('numberOfStepsCreated', function getSettings(data) {
+	numberOfStepsCreated++;
+	numberOfStepsCreated = data.numberOfStepsCreated ? 'var numberOfStepsCreated = ' + numberOfStepsCreated + ';' : 
+		`var numberOfStepsCreated = 1;`;
+});
 
+let runButton = document.getElementById('start-button');
 runButton.addEventListener('click', function() {
 	chrome.tabs.executeScript(null, {file: 'jquery.min.js'});
 	chrome.tabs.executeScript(null, {file: 'jquery-ui.min.js'});

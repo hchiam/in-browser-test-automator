3965770a2614e0c9882a98eee5eb62c9e9319c1d
hchiam <hchiam@users.noreply.github.com>
2020-12-27 19:10:04 -0500

    put var back to fix console errors

--- popup.js
+++ popup.js
@@ -7,17 +7,17 @@ chrome.storage.local.get("savedSteps", function getSettings(data) {
   let hasSteps = savedSteps.length > 0;
   let lastStep = savedSteps[savedSteps.length - 1];
   if (!hasSteps) {
-    numberOfStepsCreated_init = "let numberOfStepsCreated = 1;";
+    numberOfStepsCreated_init = "var numberOfStepsCreated = 1;";
     savedSteps_init = `
-  let savedSteps = [
+  var savedSteps = [
     {
       'action':'click',
       'value':''
     }
   ];`;
   } else {
-    numberOfStepsCreated_init = `let numberOfStepsCreated = ${savedSteps.length};`;
-    savedSteps_init = `let savedSteps = ${JSON.stringify(savedSteps)};`;
+    numberOfStepsCreated_init = `var numberOfStepsCreated = ${savedSteps.length};`;
+    savedSteps_init = `var savedSteps = ${JSON.stringify(savedSteps)};`;
   }
 });
 
